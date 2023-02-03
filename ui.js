492af9468c98fcbbe49dbd032c7bae9e22aa5bcf
Obi O'Brien <spaceman384@outlook.com>
2023-02-03 15:06:17 +1100

    Primary commit for V7.0

---
+++ ui.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.animate');
+hiddenElements.forEach((el) => observer.observe(el));
+
+screen.orientation.lock('landscape');
